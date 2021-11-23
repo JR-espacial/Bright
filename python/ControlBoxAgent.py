31a51dc8de80b244cb4ba5f103c9570157ca5c30
JARE2001 <A01701350@itesm.mx>
2021-11-22 20:42:16 -0600

    Change model
    Add trafficLights and cars to the model

--- python/ControlBoxAgent.py
+++ python/ControlBoxAgent.py
@@ -1,20 +1,19 @@
 
 from _typeshed import Self
 from mesa import Agent
-# Debido a que necesitamos que existe un solo agente por celda, elegimos ''SingleGrid''.
+from mesa import SingleGrid
     
 
 import numpy as np
 import pandas as pd
 
 class ControlBoxAgent(Agent):
-    def __init__(self, unique_id, model, trafficLights):
+    def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
-        self.trafficLights = trafficLights
-        self.cars = [0 for i in range(trafficLights)]
+       
     def optimumTrafficLight():
-        pass
+        pass    
 
     # color can be 0 for red or 1 for green
     def setTrafficLights(self,trafficLightIndex, color):
-        self.trafficLights[trafficLightIndex].setLight(color)
+        self.model.trafficLights[trafficLightIndex].setLight(color)
