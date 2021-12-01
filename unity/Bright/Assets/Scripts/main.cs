50d5d607b543568236a0b22db76d5bc759cb71b7
Mariana <a01702593@itesm.mx>
2021-12-01 17:13:26 -0600

    Send data from python

--- unity/Bright/Assets/Scripts/main.cs
+++ unity/Bright/Assets/Scripts/main.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Net;
+
 
 public class main : MonoBehaviour
 {
@@ -8,6 +10,33 @@ public class main : MonoBehaviour
     Vector3[] carPositions = {new Vector3(30, 2.5f, 2), new Vector3(-30, 2.5f, -2), new Vector3(-2, 2.5f, 28), new Vector3(2, 2.5f, -28)};
     Vector3[] carRotation = {new Vector3(0, 180, 0), new Vector3(0, 0, 0), new Vector3(0, 90, 0), new Vector3(0, -90, 0)};
     List<List<GameObject>> cars = new List<List<GameObject>>();
+
+    [System.Serializable]
+    public class TrafficLight{
+        public string message;
+        public TrafficLight(string green_){
+            message = green_;
+        }
+    }
+    [System.Serializable]
+    public class CarJSON{
+        public int destiny;
+        public int lane;
+        public bool isMoving;
+        public CarJSON(int destiny_, int lane_,bool isMoving_){
+            destiny= destiny_;
+            lane=lane_;
+            isMoving = isMoving_;
+        }
+    }
+
+    [System.Serializable]
+    public class Cars
+    {
+        public List<CarJSON> cars;
+    }
+
+
 
     private void Start() {
         for (int i = 0; i < carPositions.Length; i++)
@@ -20,7 +49,32 @@ public class main : MonoBehaviour
         newCar(1, 2);
         newCar(2, 1);
         newCar(3, 0);
+
+        string test = @"{
+                            'cars':[
+                                'car':{
+                                    'denstiny':'0',
+                                    'lane':'1',
+                                    'isMoving':'0'
+                                }
+                            ]
+                        }";
+
+        using (WebClient wc = new WebClient()){
+            var json = wc.DownloadString("https://bright-agentes.us-south.cf.appdomain.cloud/");
+            string subJson = json.Substring(1, json.Length-3);
+            TrafficLight data = JsonUtility.FromJson<TrafficLight>(subJson);
+            print(data.message);
+
+
+            Cars test2 = JsonUtility.FromJson<Cars>(test);
+            print(test2.cars[0].lane);
+        }
+
+        
+        
     }
+    
     void Update()
     {   
         //to stop car movement
